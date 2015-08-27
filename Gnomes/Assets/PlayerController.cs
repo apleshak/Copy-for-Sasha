d056e5bc8ddccf846480e1308beae8a2c9579ef0
apleshak <pleshakov.anton@gmail.com>
2015-08-26 20:48:35 -0400

    Leashing implemented in rotation

---
+++ Gnomes/Assets/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(CharacterController))]
+public class PlayerController : MyMonoBehaviour 
+{
+	public float leashLength;
+	public float moveSpeed;
+	Vector3 originalPos;
+	CharacterController playerController;
+	Rotation rotateScript;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		originalPos = transform.position;
+		originalPos.z = 0;
+		leashLength = 0.5f;
+		moveSpeed = 1.0f;
+		playerController = memoizer.GetMemoizedComponent<CharacterController>(gameObject);
+		rotateScript = memoizer.GetMemoizedComponent<Rotation>(GameObject.FindGameObjectWithTag("Sphere"));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 moveDir = GetMoveDir();
+		Vector3 currentPos = transform.position;
+		currentPos.z = 0;
+		//Debug.Log(moveDir);
+		Debug.Log(currentPos);
+		Debug.Log(originalPos);
+		
+		if (Vector3.Distance(currentPos + moveDir * Time.deltaTime, originalPos) < leashLength)
+		{
+			playerController.Move (moveDir * Time.deltaTime);
+		}
+		else
+		{
+				rotateScript.Rotate (moveDir);
+		}
+	}
+	
+	Vector3 GetMoveDir ()
+	{
+		//Prepare the mouse pointer position.
+		Vector3 mousePos = Input.mousePosition;
+		mousePos.z = Camera.main.nearClipPlane;
+		
+		//Find the mouse position in world space.
+		Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+		mouseWorldPos.z = 0;
+		
+		//Get the direction of the movement vector from original position to current mouse position.
+		return (mouseWorldPos - originalPos).normalized;
+	}
+}
---
+++ Gnomes/Assets/Rotation.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//When the object is clicked the rotation of the object changes to the direction of the click
+// with the "source" object as the origin of the direction vector to the mouse.
+public class Rotation : MonoBehaviour 
+{
+	Vector3 targetRotation;
+	public float rotationSpeed;
+	public bool rotateCoroutineRunning;
+	float rotation;
+	public Quaternion targetQuaternion;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		rotateCoroutineRunning = false;
+		rotation = 0;
+		targetQuaternion = Quaternion.identity;
+		targetRotation = Vector3.zero;
+		targetRotation.x = -1;
+		targetRotation.y = 0;
+		targetRotation.z = 0;
+		rotationSpeed = 1.0f;
+	}
+	
+	IEnumerator RotateCoroutine ()
+	{
+		rotateCoroutineRunning = true;
+		float t = 0;
+		
+		while (t/10.0f <= 1)
+		{
+			t += Time.deltaTime;
+			transform.rotation = Quaternion.Slerp(transform.rotation, targetQuaternion, t/10.0f);
+			yield return null;
+		}
+		
+		rotateCoroutineRunning = false;
+	}
+
+	// Update is called once per frame
+	public void Rotate (Vector3 dir) 
+	{
+		targetRotation = new Vector3(-dir.y, dir.x, dir.z);
+		transform.Rotate(targetRotation * rotationSpeed, Space.World);
+	}
+}
